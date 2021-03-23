2d7bae0a35d1a966efefb8c2df3c5d0127c86baa
kheintschel <78869237+kheintschel@users.noreply.github.com>
2021-03-23 17:16:54 -0500

    Update logcollector.bat
    Security Analyst changes

--- logcollector.bat
+++ logcollector.bat
@@ -1,13 +1,51 @@
 @echo off
-
-:START
-
-set INPUT=y
-echo To help us figure out the problem we would like to 
-echo collect some system information
+@setlocal enableextensions
+@cd /d "%~dp0"
+set CurrentDir=%CD%
+echo Alert Logic Support Log Collector v17.2.4 - Copyright 2019 Alert Logic, Inc. 
 echo.
+echo Please ensure that this script is being run as an Administrator 
+echo When ready, press any key to continue 
+pause > nul
+:START
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo.
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo To help us figure out the problem we would like to 
+echo collect some logs and system information. 
+echo.
+echo Upon completion, the logs will be written to folder here:
+echo.
+echo %CD%
+echo.  
+echo. 
+echo. 
+set INPUT=y
 set /P INPUT="Is this okay (Y/N)? (default=Y):"
-
 If /I "%INPUT%"=="y" GOTO:YES
 If /I "%INPUT%"=="n" GOTO:EOF
 echo.
@@ -23,15 +61,11 @@ echo.
 
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set timestamp=%%j
 set timestamp=%timestamp:~0,4%-%timestamp:~4,2%-%timestamp:~6,2%_%timestamp:~8,2%-%timestamp:~10,2%-%timestamp:~12,2%
-set targetDir=barkly_%timestamp%
+set targetDir=alertlogic_%timestamp%
 
 IF EXIST %targetDir% GOTO:RETRY_DIR
 echo Creating directory %targetDir%
 mkdir %targetDir%
-mkdir %targetDir%\daemon
-mkdir %targetDir%\logs
-mkdir %targetDir%\config
-mkdir %targetDir%\Minidump
 
 IF NOT EXIST "%WINDIR%\Temp\barklyUpgrade.log" GOTO:SKIP_UPGRADE_LOG
 echo Copying the barkly upgrade log
@@ -45,6 +79,7 @@ xcopy "%WINDIR%\MEMORY.DMP" %targetDir%
 
 IF NOT EXIST "%WINDIR%\Minidump" GOTO:SKIP_MINI_MEMORY_DUMP
 echo Copying the mini memory dump
+mkdir %targetDir%\Minidump
 xcopy /s/e "%WINDIR%\Minidump" %targetDir%\Minidump
 :SKIP_MINI_MEMORY_DUMP
 
@@ -54,18 +89,33 @@ if %OS%==64BIT set programFilesDir=%PROGRAMFILES(X86)%
 
 IF NOT EXIST "%programFilesDir%\Barkly\daemon" GOTO:SKIP_DEAMON_LOGS
 echo Copying daemon logs
+mkdir %targetDir%\daemon
 xcopy /s/e "%programFilesDir%\Barkly\daemon" %targetDir%\daemon
 :SKIP_DEAMON_LOGS
 
 IF NOT EXIST "%programFilesDir%\Barkly\logs" GOTO:SKIP_BARKLY_LOGS
-echo Copying barkly logs
-xcopy /s/e "%programFilesDir%\Barkly\logs" %targetDir%\logs
+echo Copying Barkly logs
+mkdir %targetDir%\Barkly\logs
+xcopy /s/e "%programFilesDir%\Barkly\logs" %targetDir%\Barkly\logs
 :SKIP_BARKLY_LOGS
 
 IF NOT EXIST "%programFilesDir%\Barkly\config" GOTO:SKIP_BARKLY_CONFIGS
-echo Copying barkly configs
-xcopy /s/e "%programFilesDir%\Barkly\config" %targetDir%\config
+echo Copying Barkly configs
+mkdir %targetDir%\Barkly\config
+xcopy /s/e "%programFilesDir%\Barkly\config" %targetDir%\Barkly\config
 :SKIP_BARKLY_CONFIGS
+
+IF NOT EXIST "%programFilesDir%\Alert Logic\logs" GOTO:SKIP_ALERT_LOGIC_LOGS
+echo Copying Alert Logic logs
+mkdir %targetDir%\AlertLogic\logs
+xcopy /s/e "%programFilesDir%\Alert Logic\logs" %targetDir%\AlertLogic\logs
+:SKIP_ALERT_LOGIC_LOGS
+
+IF NOT EXIST "%programFilesDir%\Alert Logic\config" GOTO:SKIP_ALERT_LOGIC_CONFIGS
+echo Copying Alert Logic configs
+mkdir %targetDir%\AlertLogic\config
+xcopy /s/e "%programFilesDir%\Alert Logic\config" %targetDir%\AlertLogic\config
+:SKIP_ALERT_LOGIC_CONFIGS
 
 echo Generating system report
 msinfo32 /report %targetDir%\msinfo-report.txt
@@ -76,11 +126,10 @@ wevtutil.exe epl System %targetDir%\System.evtx
 echo Saving Application Log
 wevtutil.exe epl Application %targetDir%\Application.evtx
 
-
 echo.
 echo.
 echo Everything has been copied to the folder %targetDir%.
-echo If you could zip that up and send it to Barkly we would
+echo If you could zip that up and send it to Alert Logic we would
 echo greatly appreciate it.  Thanks for helping us!
 echo.
 echo Press any key to exit.
